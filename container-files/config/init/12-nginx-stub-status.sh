1609b6b389761139b922743e25091bd01e257f5e
ryzy <marcin@ryzycki.com>
2016-04-16 17:23:14 +0100

    Display info about Nginx status page only in log console, not in TTY mode

--- container-files/config/init/12-nginx-stub-status.sh
+++ container-files/config/init/12-nginx-stub-status.sh
@@ -3,4 +3,5 @@
 # Configure access IP address for nginx status page under /nginx_status
 #
 sed -i "s|allow 127.0.0.1|allow ${STATUS_PAGE_ALLOWED_IP}|g" /etc/nginx/conf.d/stub-status.conf
-echo "Nginx status page: allowed address set to $STATUS_PAGE_ALLOWED_IP."
+# Only echo this message to log console, not when in TTY mode.
+[ -t 0 ] || echo "Nginx status page: allowed address set to $STATUS_PAGE_ALLOWED_IP."
